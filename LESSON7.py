ddc43c4b326a1b0edf102d9ec84764ab5d196d5e
c2222 <bladrizzo@gmail.com>
2025-03-29 16:28:35 +0200

    Lesson 2.
    Creating the Student class and creating 2 of its objects

--- LESSON7.py
+++ LESSON7.py
@@ -27,35 +27,55 @@
 
 
 
-class Counter:
-    def __init__(self, max_number):
-        self.i = 0
-        self.max_number = max_number
+# class Counter:
+#     def __init__(self, max_number):
+#         self.i = 0
+#         self.max_number = max_number
+#
+#     def __iter__(self):
+#         self.i = 0
+#         return self
+#
+#     def __next__(self):
+#         self.i += 1
+#         if self.i > self.max_number:
+#             raise StopIteration
+#         return self.i
+#
+# cont = Counter(5)
+#
+# for elem in cont:
+#     print(elem)
+#
+# print(30)
+#
+# cont = Counter(5)
+#
+#
+# print(+38)
+#
+# cont = Counter(5)
+#
+# print(next(cont))
+# print(next(cont))
+#
+# list_power_2 = [1 ** 2 for i in range(1,20,1) ]
+# print(list_power_2)
+#
+# def raise_to_the_degrees(number, max_degrees):
+#     for i in range(max_degrees + 1):
+#         yield (number ** i)
+#
+# res = raise_to_the_degrees(2, 500)
+# print(res)
 
-    def __iter__(self):
-        self.i = 0
-        return self
 
-    def __next__(self):
-        self.i += 1
-        if self.i > self.max_number:
-            raise StopIteration
-        return self.i
+class Helper:
+    def __init__(self, work):
+        self.work = work
 
-cont = Counter(5)
+    def __call__(self, work):
+        return f"I will help you with your {self.work}. Afterwards I will help you with {work}."
 
-for elem in cont:
-    print(elem)
-
-print(30)
-
-cont = Counter(5)
-
-
-print(+38)
-
-cont = Counter(5)
-
-print(next(cont))
-print(next(cont))
-
+helper = Helper("homework")
+print(helper("cleaning"))
