c3c13b0caa6ed3d5202609f8d4b3f6192ce48f92
c2222 <bladrizzo@gmail.com>
2025-04-05 13:14:11 +0300

    Lesson 2.
    Creating the Student class and creating 2 of its objects

---
+++ Dz5.py
@@ -0,0 +1,25 @@
+def calculator_decorator(func):
+    def wrapper(expression):
+        try:
+            result = func(expression)
+            print(f'Результат: {result}')
+            return result
+        except ZeroDivisionError:
+            print('Помилка: Ділення на нуль!')
+        except SyntaxError:
+            print('Помилка: Неправильний синтаксис виразу!')
+        except NameError:
+            print('Помилка: Невідомі символи у виразі!')
+        except Exception as e:
+            print(f'Інша помилка: {e}')
+    return wrapper
+
+@calculator_decorator
+def calculate(expression):
+    return eval(expression)
+
+# Приклади виклику
+calculate("10 + 5")
+calculate("10 / 0")
+calculate("10 +")
+calculate("abc + 1")
